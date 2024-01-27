1089511d7cfbc680525c979b08460fc186e14b99
Mateus dantas <mateusdantz@gmail.com>
2024-01-26 21:36:21 -0300

    Adicionando pasta arrays e incluindo exercicio 1

---
+++ javascript/arrays/exercicioTabuada.js
@@ -0,0 +1,9 @@
+// Crie 1 programa que dado um numero imprima a tabuada deste numero
+
+const numero = 8;
+
+for (let i = 1; i <= 10; i++){
+
+    console.log(`${numero} x ${i} = ${numero*i}`)
+   
+}
